3baf3fd5fa13a4d6d5aa76a11c54203f39638f01
Tusharbalgobind <157356488+Tusharbalgobind@users.noreply.github.com>
2024-02-02 14:59:25 +0200

    Update Graph.js

--- screens/Graph.js
+++ screens/Graph.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, FlatList, Image, Modal } from 'react-native';
 import { connect } from 'react-redux';
 import { setTradeModelVisibility } from "../stores/tab/tabActions";
