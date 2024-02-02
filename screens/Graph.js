dac0096cce487cca7a203ad0e163f807800e91a0
Tusharbalgobind <157356488+Tusharbalgobind@users.noreply.github.com>
2024-02-02 15:01:12 +0200

    Update Graph.js

--- screens/Graph.js
+++ screens/Graph.js
@@ -10,6 +10,22 @@ import { IconTextButton } from '../components';
 import { SIZES, COLORS, FONTS, dummyData, icons } from '../constants';
 import { WebView } from 'react-native-webview';
 
+useEffect(() => {
+        {/* Retrieve access token */}
+      const getAccessToken = async () => {
+        {/* Retrieving token from AsyncStorage */}
+        const accessToken = await AsyncStorage.getItem('accessToken');
+        {/* Checking whether the token is valid */}
+        if (accessToken) {
+          console.log('User logged in successfully');
+          console.log(accessToken);
+          navigation.navigate('Home');
+        } else {
+          console.log('User is not logged in');
+          setUrl(`https://api.kraken.com/0/oauth2/authorize?client_id=${krakenClientId}&response_type=code&redirect_uri=${krakenRedirectUri}&scope=public%20private`);
+        }
+      };
+
 const Graph = () => {
 
     function handleView () {
